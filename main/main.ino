31a60c0202b58c9bc08243c642282c832ab9e854
arachid1 <alilerachidi@gmail.com>
2019-12-03 15:21:49 -0500

    main.py

--- main/main.ino
+++ main/main.ino
@@ -16,10 +16,10 @@ byte colPins[COLS] = {5, 4, 3};
 
 //Keypad customKeypad = Keypad(makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS); 
 
-int enable1 = 11;
-int enable2 = 10;
-int A = 1;
-int B = 2;
+int enable_left = 11;
+int enable_right = 10;
+int L = 1;
+int R = 2;
 int leftPin = A0; 
 
 int echoPin = 13;
@@ -32,19 +32,26 @@ float duration, distance;
 
 void setup() {
   Serial.begin(9600); 
-  pinMode(A, OUTPUT);
-  pinMode(B, OUTPUT);
+  pinMode(L, OUTPUT);
+  pinMode(R, OUTPUT);
   pinMode(echoPin, INPUT);
   pinMode(trigPin, OUTPUT);
-  digitalWrite(A, LOW);
-  digitalWrite(B, LOW);
+  rbt_forward(255);
 }
 
 void loop() {
-  delay(4000);
-  digitalWrite(A, LOW);
-  digitalWrite(B, HIGH);
-  Serial.print("hey");
+
+//  turn_left();
+//  delay(2500);
+  turn_right();
+  delay(2500);
+  
+//  delay(3000);
+//  rbt_stop();
+//  delay(2000);
+//  turn_left();
+  // rbt_enable(150, 150);
+  // delay(3000);
 //  if (customKey1 == '9') {
 //    digitalWrite(A, LOW);
 //    digitalWrite(B, HIGH);
@@ -78,17 +85,35 @@ double microsecondsToInches(double microseconds) {
   return microseconds / 74 / 2;
 }
 
+void rbt_move() {
+
+  digitalWrite(L, HIGH);
+  digitalWrite(R, LOW);
+}
+
 void rbt_stop() {
-  digitalWrite(A, LOW);
-  digitalWrite(B, LOW);
+  digitalWrite(L, HIGH);
+  digitalWrite(R, HIGH);
 }
 
-void rbt_forward() {
-  digitalWrite(A, LOW);
-  digitalWrite(B, HIGH);
+void rbt_forward(int value) {
+  analogWrite(enable_left, value); 
+  analogWrite(enable_right, value);
+  rbt_move();
 }
 
-void rbt_left(int left, int right) {
-  analogWrite(enable1, left); // left
-  analogWrite(enable2, right);
+void turn_left() {
+  rbt_move();
+  analogWrite(enable_left, 255);
+  analogWrite(enable_right, 0);
+  delay(2080);
+  rbt_stop();
+}
+
+void turn_right() {
+  rbt_move();
+  analogWrite(enable_right, 255);
+  analogWrite(enable_left, 0);
+  delay(1860);
+  rbt_stop();
 }
