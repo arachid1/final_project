6b508a7c377c513c1f018d18c91238b46fef6eba
arachid1 <alilerachidi@gmail.com>
2019-12-03 19:49:52 -0500

    UPDATED main.py

--- main/main.ino
+++ main/main.ino
@@ -1,8 +1,8 @@
 //#include <Keypad.h>
 //#include <NewPing.h>
 
-const byte ROWS = 4; 
-const byte COLS = 3; 
+const byte ROWS = 4;
+const byte COLS = 3;
 
 char hexaKeys[ROWS][COLS] = {
   {'1', '2', '3'},
@@ -11,16 +11,18 @@ char hexaKeys[ROWS][COLS] = {
   {'*', '0', '#'}
 };
 
-byte rowPins[ROWS] = {9, 8, 7, 6}; 
-byte colPins[COLS] = {5, 4, 3}; 
+byte rowPins[ROWS] = {9, 8, 7, 6};
+byte colPins[COLS] = {5, 4, 3};
 
-//Keypad customKeypad = Keypad(makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS); 
+int old_distance = -1;
+
+//Keypad customKeypad = Keypad(makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS);
 
 int enable_left = 11;
 int enable_right = 10;
 int L = 1;
 int R = 2;
-int leftPin = A0; 
+int leftPin = A0;
 
 int echoPin = 13;
 int trigPin = 12;
@@ -33,84 +35,82 @@ long duration, cm, start, ending;
 //float duration, distance;
 
 void setup() {
-  Serial.begin(9600); 
+  Serial.begin(9600);
   pinMode(L, OUTPUT);
   pinMode(R, OUTPUT);
-  pinMode(echoPin, INPUT);
-  pinMode(trigPin, OUTPUT);
-  rbt_forward(255);
+  //rbt_forward(255);
 }
 
 void loop() {
 
-detect_wall();
-
-//  turn_left();
-//  delay(2500);
-//  turn_right();
-//  delay(2500);
-
-//  delay(3000);
-//  rbt_stop();
-//  delay(2000);
-//  turn_left();
-  // rbt_enable(150, 150);
-  // delay(3000);
-//  if (customKey1 == '9') {
-//    digitalWrite(A, LOW);
-//    digitalWrite(B, HIGH);
-//
-//    int lSensor = analogRead(A0);
-//
-//    if (lSensor < 200) {
-//      analogWrite (enable1, 255); // left
-//      analogWrite (enable2, 240);
-//    }
-//    else {
-//      delay(50);
-//
-//      duration = sonar.ping();
-//      distance = (duration / 2) * .0343;
-//
-//      if (distance < 10) {
-//        analogWrite (enable1, 0);
-//        analogWrite (enable2, 240);
-//      }
-//    }
-//  }
-}
-
-// BOLAJI: write function that uses left sensor to detect wall and returns that distance
-void detect_wall() {
-  pinMode(trigPin, OUTPUT);
-  pinMode(echoPin, INPUT);
-  digitalWrite(trigPin, LOW);
-  delayMicroseconds(2);
-  digitalWrite(trigPin, HIGH);
-  delayMicroseconds(5);
-  digitalWrite(trigPin, LOW);
-  
-  pinMode(trigPin, INPUT);
-  //duration = pulseIn(trigPin, HIGH);
-  while (digitalRead(echoPin) == LOW) {
-  }
-  start = micros();
-  while (digitalRead(echoPin) == HIGH) {
-  }
-  ending = micros();
-  duration = ending - start;
-  // convert the time into a distance
-  cm = microsecondsToCentimeters(duration);
-  
-  Serial.print(cm);
+  rbt_forward(255);
+  int distance = detect_wall();
+  Serial.print(distance);
   Serial.print("cm");
   Serial.println();
-
-  delay(100);
-
-  if (cm > 12) {
-    turn_left();
+  if (abs(distance - old_distance) > 10) {
+    if (old_distance != -1) {
+      Serial.println("here");
+      rbt_stop();
+      //      Serial.print("turn right");
+      //      turn_right();
+      //      rbt_stop();
+      delay(5000);
+    }
   }
+  old_distance = distance;
+  
+
+  //  if (customKey1 == '9') {
+  //    digitalWrite(A, LOW);
+  //    digitalWrite(B, HIGH);
+  //
+  //    int lSensor = analogRead(A0);
+  //
+  //    if (lSensor < 200) {
+  //      analogWrite (enable1, 255); // left
+  //      analogWrite (enable2, 240);
+  //    }
+  //    else {
+  //      delay(50);
+  //
+  //      duration = sonar.ping();
+  //      distance = (duration / 2) * .0343;
+  //
+  //      if (distance < 10) {
+  //        analogWrite (enable1, 0);
+  //        analogWrite (enable2, 240);
+  //      }
+  //    }
+  //  }
+}
+
+int detect_wall() {
+
+  int cm = 0;
+  for (int i = 0; i < 15; i++) {
+    pinMode(echoPin, INPUT);
+    pinMode(trigPin, OUTPUT);
+    digitalWrite(trigPin, LOW);
+    delayMicroseconds(2);
+    digitalWrite(trigPin, HIGH);
+    delayMicroseconds(5);
+    digitalWrite(trigPin, LOW);
+
+    pinMode(trigPin, INPUT);
+
+    while (digitalRead(echoPin) == LOW) {
+    }
+    start = micros();
+    while (digitalRead(echoPin) == HIGH) {
+    }
+    ending = micros();
+    duration = ending - start;
+    // convert the time into a distance
+    cm += microsecondsToCentimeters(duration);
+  }
+
+  return cm / 10;
 }
 
 double microsecondsToInches(double microseconds) {
@@ -136,7 +136,7 @@ void rbt_stop() {
 }
 
 void rbt_forward(int value) {
-  analogWrite(enable_left, value); 
+  analogWrite(enable_left, value);
   analogWrite(enable_right, value);
   rbt_move();
 }
@@ -155,4 +155,4 @@ void turn_right() {
   analogWrite(enable_left, 0);
   delay(1860);
   rbt_stop();
-} 
+}
